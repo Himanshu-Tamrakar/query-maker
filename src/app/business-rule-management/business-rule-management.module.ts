f2e68afa6510873f5e5b861a814c8d9ae7ed472f
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2021-10-06 10:22:57 +0530

    components

--- src/app/business-rule-management/business-rule-management.module.ts
+++ src/app/business-rule-management/business-rule-management.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CreateRuleComponent } from './create-rule/create-rule.component';
 import { BusinessRulesComponent } from './business-rules/business-rules.component';
 import { QueryBuilderModule } from '../query-builder/query-builder.module';
+import { CardViewComponent } from './card-view/card-view.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { RuleDetailsComponent } from './rule-details/rule-details.component';
 
 @NgModule({
-  declarations: [CreateRuleComponent, BusinessRulesComponent],
+  declarations: [CreateRuleComponent, BusinessRulesComponent, CardViewComponent, ListViewComponent, RuleDetailsComponent],
   imports: [CommonModule, QueryBuilderModule],
   exports: [CreateRuleComponent],
 })
